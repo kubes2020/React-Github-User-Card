0310af782f7d9bc6bcbe7fe290fc1830e5c41ee4
Brian Kubes <briankubes@gmail.com>
2020-09-29 12:40:05 -0700

    data is going to Card

--- card/src/App.js
+++ card/src/App.js
@@ -1,21 +1,27 @@
-import Axios from "axios";
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import Card from "./components/Card";
 
 class App extends React.Component {
-  state = {
-    followers: [],
-  };
+  constructor() {
+    super();
+    this.state = {
+      followers: [],
+    };
+  }
 
   componentDidMount() {
     axios
       .get("https://api.github.com/users/kubes2020/followers")
       .then((res) => {
         console.log("gh response", res.data);
+        this.setState({
+          followers: res.data,
+        });
       })
       .catch((err) => {
-        console.log("something wong", EvalError);
+        console.log("something wong", err);
       });
   }
 
@@ -23,6 +29,7 @@ class App extends React.Component {
     return (
       <>
         <h1>Followers</h1>
+        <Card allFollowers={this.state.followers} />
       </>
     );
   }
