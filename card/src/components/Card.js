9bc22a4f01bea4df71c460f0988f615011daf95e
Brian Kubes <briankubes@gmail.com>
2020-09-29 12:49:38 -0700

    pics and name are rendering on Card

--- card/src/components/Card.js
+++ card/src/components/Card.js
@@ -6,6 +6,12 @@ class Card extends React.Component {
     return (
       <>
         <h2>Card</h2>
+        {this.props.allFollowers.map((fol) => (
+          <div className="card-container">
+            <p>{fol.login}</p>
+            <img src={fol.avatar_url} key={fol.id} alt="pic" />
+          </div>
+        ))}
       </>
     );
   }
