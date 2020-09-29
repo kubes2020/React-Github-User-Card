d7de1f06ec0662777a9b3eeff05eeedd9420b7e9
Brian Kubes <briankubes@gmail.com>
2020-09-29 13:27:19 -0700

    just changed axios to a function call

--- card/src/App.js
+++ card/src/App.js
@@ -8,12 +8,19 @@ class App extends React.Component {
     super();
     this.state = {
       followers: [],
+      user: "kubes2020",
     };
   }
 
+  // user = "kubes2020";
+
   componentDidMount() {
+    this.getFollowers(this.state.user);
+  }
+
+  getFollowers = (user) => {
     axios
-      .get("https://api.github.com/users/kubes2020/followers")
+      .get(`https://api.github.com/users/${user}/followers`)
       .then((res) => {
         console.log("gh response", res.data);
         this.setState({
@@ -23,12 +30,35 @@ class App extends React.Component {
       .catch((err) => {
         console.log("something wong", err);
       });
-  }
+  };
+
+  onChange = (e) => {
+    e.persist();
+    this.setState({
+      user: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
 
   render() {
     return (
       <>
         <h1>Followers</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="user">
+            Name of User
+            <input
+              type="text"
+              id="user"
+              onChange={this.onChange}
+              value={this.state.user}
+            ></input>
+          </label>
+          <button>Submit</button>
+        </form>
         <Card allFollowers={this.state.followers} />
       </>
     );
