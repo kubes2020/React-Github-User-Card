54f87f18a90029aebc7c1fa8498a44d855638a4d
Brian Kubes <briankubes@gmail.com>
2020-09-29 15:28:47 -0700

    working except need to add spread opperator

--- card/src/App.js
+++ card/src/App.js
@@ -8,14 +8,25 @@ class App extends React.Component {
     super();
     this.state = {
       followers: [],
-      user: "kubes2020",
+      user: "",
     };
   }
 
   // user = "kubes2020";
 
   componentDidMount() {
-    this.getFollowers(this.state.user);
+    console.log("comp did mount");
+    axios
+      .get("https://api.github.com/users/kubes2020")
+      .then((res) => {
+        console.log("gh response", res.data);
+        this.setState({
+          followers: [res.data],
+        });
+      })
+      .catch((err) => {
+        console.log("something wong", err);
+      });
   }
 
   getFollowers = (user) => {
@@ -41,9 +52,11 @@ class App extends React.Component {
 
   onSubmit = (e) => {
     e.preventDefault();
+    this.getFollowers(this.state.user);
   };
 
   render() {
+    console.log("did render");
     return (
       <>
         <h1>Followers</h1>
